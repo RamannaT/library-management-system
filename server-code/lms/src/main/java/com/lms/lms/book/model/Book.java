71544c8b672b9f9de480cd86c1e3725e85350011
RamannaT <ramannatatagar@gmai.com>
2020-04-30 14:33:15 +0530

    server-side rest-controller is addded

---
+++ server-code/lms/src/main/java/com/lms/lms/book/model/Book.java
@@ -0,0 +1,12 @@
+package com.lms.lms.book.model;
+
+public class Book {
+	
+	public Book(){}
+	
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+
+}
---
+++ server-code/lms/src/main/java/com/lms/lms/library/model/Library.java
@@ -0,0 +1,13 @@
+package com.lms.lms.library.model;
+
+public class Library {
+
+	public Library() {
+	}
+
+	@Override
+	public String toString() {
+
+		return super.toString();
+	}
+}
